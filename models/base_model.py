7b31f6cfed81a51f856492f0ecc675ab3a56f006
arwaa29 <arwahesham73@gmail.com>
2024-02-08 13:44:41 +0200

    a

--- models/base_model.py
+++ models/base_model.py
@@ -47,6 +47,6 @@ class BaseModel:
                                                         of ___dict__"""
             my_dict = self.__dict__.copy()
             my_dict['__class__'] = self.__class__.__name__
-            my_dict['created_at'] = self.created_at.isoforamat()
-            my_dict['updated_at'] = self.updated_at.isoformate()
+            my_dict['created_at'] = self.created_at.isoformat()
+            my_dict['updated_at'] = self.updated_at.isoformat()
             return (my_dict)

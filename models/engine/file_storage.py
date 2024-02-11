9058d76e1bf29e52752d55799b318419d17a3462
elsaaeid <saidsadaoy@gmail.com>
2024-02-10 17:27:37 -0800

    a

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,7 +40,9 @@ class FileStorage:
 
     def classesReload(self):
         """ Returns a dictionary of valid classes and their references """
-        classesReload = {"BaseModel": BaseModel,"User": User,"State": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+        classesReload = {"BaseModel": BaseModel, "User": User,
+                "State": State, "City": City, "Amenity": Amenity,
+                "Place": Place, "Review": Review}
         return classesReload
 
     def reload(self):

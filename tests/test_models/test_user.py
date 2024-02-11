fc8c0e949865427fffaa5900cef2883651962e01
elsaaeid <saidsadaoy@gmail.com>
2024-02-11 10:14:30 -0800

    a

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import unittest
 import os
+import pep8
 from models.user import User
 from models.engine.file_storage import FileStorage
 
@@ -13,6 +14,16 @@ def setUpModule():
 def tearDownModule():
     """ Function to clean up a Module"""
     pass
+
+class TestStringMethods(unittest.TestCase):
+    """ Check the pep8 """
+    def testpep8(self):
+        style = pep8.StyleGuide(quiet=True)
+        file1 = "models/user.py"
+        file2 = "tests/test_models/test_user.py"
+        check = style.check_files([file1, file2])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warning).")
 
 class TestModels(unittest.TestCase):
     """ Funtion to test the BaseModel"""

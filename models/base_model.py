b426aa5dcbc1db97d3b0bb9eb8c5c2035afbf972
elsaaeid <saidsadaoy@gmail.com>
2024-02-13 05:00:55 -0800

    a

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel:
         def __str__(self):
             """returns string representation"""
             return ("[{}] ({}) {}".format(type(self).__class__.__name__,
-                                                self.id, self.__dict__))
+                    self.id, self.__dict__))
 
         def save(self):
             """updates the update_at"""
